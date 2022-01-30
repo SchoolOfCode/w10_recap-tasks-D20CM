399169a9bcc1de0524e481db8f5d9a51c501f38e
D20CM <scottbrown500@hotmail.com>
2022-01-30 20:06:10 +0000

    add image to login screen and header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,6 +5,7 @@ import Articles from "../Articles/Articles";
 import LoginButton from "../Login/Login";
 import LogoutButton from "../Logout/Logout";
 import { useAuth0 } from "@auth0/auth0-react";
+import pigeon from "../../images/pigeon.png";
 
 function App() {
   const [articles] = useState(dataset);
@@ -12,17 +13,24 @@ function App() {
 
   return isAuthenticated ? (
     <main className={css.App}>
-      <h1>WikiPigeon</h1>
+      <header>
+        <h1>WikiPigeon</h1>{" "}
+        <img src={pigeon} alt="pigeon" className={css.pigeon} />
+      </header>
       <LoginButton />
       <LogoutButton />
       <Articles articles={articles} />
     </main>
   ) : (
     <main className={css.App}>
-      <h1>WikiPigeon</h1>
-      <p>Please log in to see the best pigeon discussion in town.</p>
+      <header>
+        <h1>WikiPigeon</h1>{" "}
+      </header>
       <LoginButton />
       <LogoutButton />
+      <p>Please log in to see the best pigeon discussion in town.</p>
+
+      <img src={pigeon} alt="pigeon" className={css.pigeonLarge} />
     </main>
   );
 }
